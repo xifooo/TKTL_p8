ffa5964ac9e60464c38fed78535e5b2c1f6f1f87
xifoo <xifoo@gmail.com>
2024-04-19 22:45:40 +0800

    8.6 complete

--- library-backend.js
+++ library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require("uuid")
 
 let authors = [
   {
@@ -120,6 +121,14 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [allAuthors!]!
   }
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
   `
   
   const resolvers = {
@@ -147,6 +156,18 @@ const typeDefs = `
         }
         return allAuthors
       }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      books.push({...args})
+      if (!authors.includes(args.author)){
+        authors.push({
+          name: args.author,
+          born: null, 
+          id: uuid()
+        })
+      }
+    }
   }
 }
 
